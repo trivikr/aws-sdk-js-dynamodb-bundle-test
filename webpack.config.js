a4f58af7f4cebedeac4a1cdaac20ac3bff8a3b0b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-25 13:29:05 -0700

    checkpoint: add lambda functions with webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+const glob = require("glob");
+const entryArray = glob.sync("./src/*.ts");
+
+module.exports = {
+  entry: entryArray.reduce((acc, item) => {
+    const name = item.replace("./src/", "").replace(".ts", "");
+    acc[name] = item;
+    return acc;
+  }, {}),
+
+  output: {
+    filename: "[name].js"
+  },
+
+  // Resolve .ts and .js extensions
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+
+  // Target node
+  target: "node",
+
+  // Set the webpack mode
+  mode: process.env.NODE_ENV || "production",
+
+  // Add the TypeScript loader
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
